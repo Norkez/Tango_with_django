a986fca926281656a5090b362e7a23c13b43d3fc
Norkez <yellrimp@gmail.com>
2018-04-02 10:13:17 +0900

    wehose API 적용

--- rango/wehose_search.py
+++ rango/wehose_search.py
@@ -19,4 +19,37 @@ def read_webhose_key():
         raise IOError('search.key file not found')
 
     return webhose_api_key
+
+def run_query(search_terms, szie=10):
+    """
+    """
+    webhose_api_key = read_webhose_key
+
+    if not webhose_api_key:
+        raise KeyError('Webhose key not found')
+
+    root_url = 'http://webhose.io/search'
+
+    query_string = urllib.parse.quote(search_terms)
+
+    search_url = ('{root_url}?token={key}&foramt=json&q={query}'
+                    '&sort=relevancy&size={size}').foramt(
+                        root_url=root_url,
+                        key=webhose_api_key,
+                        query=query_string,
+                        size=size)
+    results = []
+
+    try:
+        reponse = urllib.request.urlopen(search_url).read().decode('utf-8')
+        json_response = json.loads(response)
+
+        for post in json_response['posts']:
+            results.append({'title': post['title'],
+                            'link': post['url'],
+                            'summary': post['text'][:200]})
+    except:
+        print("Error when querying the Webhose API")
+
+    return results
     
